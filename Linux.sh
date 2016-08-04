31dad7860ee97b8434450d640ad32c44c8ca0509
CJ-Chen <120509419@qq.com>
2016-08-04 16:30:53 +0800

    a big step

--- Linux.sh
+++ Linux.sh
@@ -3,10 +3,10 @@
 #This script is intended for launch on *nix machines
 
 #-Xmx4000m indicates 4000 mb of memory, adjust number up or down as needed
-#Script must be in the same directory as igv.jar
+#Script must be in the same directory as TBtools.jar
 #Add the flag -Ddevelopment = true to use features still in development
 prefix=`dirname $(readlink $0 || echo $0)`
 exec java -Xmx4000m \
 	-Dapple.laf.useScreenMenuBar=true \
 	-Djava.net.preferIPv4Stack=true \
-	-jar "$prefix"/bin/TBtools_v0.296.jar "$@"
+	-jar "$prefix"/bin/TBtools_v0.297.jar "$@"
