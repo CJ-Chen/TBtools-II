0f60ab1151fcae13a5c4f2318cdf8476f0a66bdb
CJ-Chen <120509419@qq.com>
2016-08-05 21:03:16 +0800

    add script to help some users

---
+++ Linux.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#This script is intended for launch on *nix machines
+
+#-Xmx4000m indicates 4000 mb of memory, adjust number up or down as needed
+#Script must be in the same directory as TBtools.jar
+#Add the flag -Ddevelopment = true to use features still in development
+prefix=`dirname $(readlink $0 || echo $0)`
+exec java -Xmx4000m \
+	-Dapple.laf.useScreenMenuBar=true \
+	-Djava.net.preferIPv4Stack=true \
+	-jar "$prefix"/TBtools_v0.299.jar "$@"
