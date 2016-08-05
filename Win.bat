e6fece8bdf8feff7192ca37caf779b225406723e
CJ-Chen <120509419@qq.com>
2016-08-05 08:28:32 +0800

    version up

--- Win.bat
+++
@@ -1,4 +0,0 @@
-::Get the current batch file's short path
-for %%x in (%0) do set BatchPath=%%~dpsx
-for %%x in (%BatchPath%) do set BatchPath=%%~dpsx
-java  -Xmx1200m -Dproduction=true -Djava.net.preferIPv4Stack=true -Dsun.java2d.noddraw=true -jar %BatchPath%\bin\TBtools_v0.297.jar  %*
