83f9930d1aa2057336084c89bd035dd611db4872
CJ-chen <120509419@qq.com>
2016-09-04 18:46:38 +0800

    合并或者说重写了两个表格提取工具，增加了两个大文件极速查看插件，支持gz压缩，查看fq.gz或者注释表格等大文件可用

--- Win.bat
+++ Win.bat
@@ -1,4 +1,4 @@
 ::Get the current batch file's short path
 for %%x in (%0) do set BatchPath=%%~dpsx
 for %%x in (%BatchPath%) do set BatchPath=%%~dpsx
-java  -Xmx1200m -Dproduction=true -Djava.net.preferIPv4Stack=true -Dsun.java2d.noddraw=true -jar %BatchPath%\TBtools_v0.394.jar %*
+java  -Xmx1200m -Dproduction=true -Djava.net.preferIPv4Stack=true -Dsun.java2d.noddraw=true -jar %BatchPath%\TBtools_v0.396.jar %*
